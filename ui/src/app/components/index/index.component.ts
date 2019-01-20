6dbbe81aef0ccdf1e30dfbcd06e110a13d05c382
rabik <rezhake.rebike@citi.com>
2019-01-20 00:44:15 -0600

    Removed the default fields from index component

--- ui/src/app/components/index/index.component.ts
+++ ui/src/app/components/index/index.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { IpcService } from "../../services/ipc.service";
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.scss']
 })
-export class IndexComponent {
-  title = 'mockapi';
+export class IndexComponent implements OnInit {
+
+    constructor(private _ipc: IpcService) { }
+
+    ngOnInit() { }
+
 }
